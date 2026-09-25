fab20b6a204b61f11bb7ed5db4d0603fc4c51d17
Tim Hilt <timhilt@live.de>
2026-09-25 21:18:09 +0200

    small adaptations

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = 'Tim Hilt';
 export const SITE_DESCRIPTION = 'Personal blog of Tim Hilt.';
+
+export const GITHUB_URL = 'https://github.com/tim-hilt';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/tim-hilt-2958a11b7/';
